266362634096b292b503261ecb6cf036ff5336bc
Izaac <izaac.kirscht@gmail.com>
2017-12-01 14:07:36 -0600

    Update MFRC522.py

--- MFRC522/MFRC522.py
+++ MFRC522/MFRC522.py
@@ -287,7 +287,7 @@ class MFRC522:
     (status, backData, backLen) = self.MFRC522_ToCard(self.PCD_TRANSCEIVE, buf)
     
     if (status == self.MI_OK) and (backLen == 0x18):
-      print "Size: " + str(backData[0])
+      #print "Size: " + str(backData[0])
       return    backData[0]
     else:
       return 0
@@ -340,7 +340,8 @@ class MFRC522:
       print "Error while reading!"
     i = 0
     if len(backData) == 16:
-      print "Sector "+str(blockAddr)+" "+str(backData)
+      #print "Sector "+str(blockAddr)+" "+str(backData)
+      print " "
   
   def MFRC522_Write(self, blockAddr, writeData):
     buff = []
